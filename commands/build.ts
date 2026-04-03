244c5ffdd6b102ad2f9ee9e793ba7638b2a1b1b9
roll <eskarev@gmail.com>
2026-04-03 17:29:13 +0100

    Implemented serve and build commands with Vite

--- commands/build.ts
+++ commands/build.ts
@@ -1,8 +1,13 @@
 import { Command } from "commander"
+import { build as viteBuild } from "vite"
 
 /**
  * Build the site for production.
  */
 export const build = new Command("build")
   .description("Build the site for production")
-  .action(() => {})
+  .action(async () => {
+    await viteBuild({
+      configFile: "website/vite.config.ts",
+    })
+  })
--- commands/serve.ts
+++ commands/serve.ts
@@ -1,8 +1,17 @@
 import { Command } from "commander"
+import { createServer } from "vite"
 
 /**
  * Start a development server.
  */
 export const serve = new Command("serve")
   .description("Start a development server")
-  .action(() => {})
+  .action(async () => {
+    const server = await createServer({
+      configFile: "website/vite.config.ts",
+      server: { port: 8000, host: true },
+    })
+    await server.listen()
+    server.printUrls()
+    server.bindCLIShortcuts({ print: true })
+  })
